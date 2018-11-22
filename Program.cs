da4e4e39f45954b99956a4a5124960094a6cfb88
ahkhawer <ahjavediqbal@gmail.com>
2018-11-22 14:47:01 +0500

    Program.cs modified

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace DotNetHello
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!")
         }
     }
 }
